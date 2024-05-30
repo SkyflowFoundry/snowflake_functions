5101580e5864b6fb785918a8fa3b5818074883a0
samsternberg <sam.sternberg@skyflow.com>
2024-05-29 22:15:08 -0400

    enable vault and table creation from UDF

--- snowflake_udf_demo/skyflow_udf_demo.sql
+++ snowflake_udf_demo/skyflow_udf_demo.sql
@@ -2,45 +2,47 @@
 USE DATABASE SKYFLOW_DEMO;
 
 -- Step 1: Check table to see initial plain text PII
-SELECT * FROM DOCTORS_DEMO;
+SELECT * FROM CUSTOMERS;
 
 -- Step 2: Tokenize the table, passing in table name as a parameter, and query the table again.
-CALL SKYFLOW_TOKENIZE_TABLE('DOCTORS_DEMO');
+--   SKYFLOW_TOKENIZE_TABLE(VAULT_NAME, TABLE_NAME, PRIMARY_KEY, 'PII_COL1,PII_COL2,PII_COL3,PII_COL4,...', 'VAULT_OWNER_EMAIL');
 
--- Step 3a: Run a sample query without detokenizing, for example the doctors who started after year 2010. PII will be tokenized.
-SELECT  DOCTOR_ID,
+CALL SKYFLOW_TOKENIZE_TABLE('SkyflowVault', 'CUSTOMERS', 'CUSTOMER_ID', 'NAME,EMAIL,PHONE,ADDRESS', 'yourname@yourdomain.com');
+
+-- Step 3a: Run a sample query without detokenizing, for example the customers who joined after year 2010. PII will be tokenized.
+SELECT  CUSTOMER_ID,
         NAME,
         EMAIL,
-        SPECIALTY,
-        EMPLOYMENT_START_DATE
-FROM DOCTORS_DEMO
-WHERE EMPLOYMENT_START_DATE > '2010';
+        LIFETIME_PURCHASE_AMOUNT,
+        CUSTOMER_SINCE
+FROM CUSTOMERS
+WHERE CUSTOMER_SINCE > '2010';
 
 -- Step 3b: Now run the query using Skyflow detokenize to obtain PII data.
-SELECT  DOCTOR_ID,
+SELECT  CUSTOMER_ID,
         SKYFLOW_DETOKENIZE(NAME),
         SKYFLOW_DETOKENIZE(EMAIL),
-        SPECIALTY,
-        EMPLOYMENT_START_DATE
-FROM DOCTORS_DEMO
-WHERE EMPLOYMENT_START_DATE > '2010';
+        LIFETIME_PURCHASE_AMOUNT,
+        CUSTOMER_SINCE
+FROM CUSTOMERS
+WHERE CUSTOMER_SINCE > '2010';
 
 -- Step 4: Make some updates to the data
-DELETE FROM DOCTORS_DEMO WHERE DOCTOR_ID = 1 OR DOCTOR_ID = 2; -- Delete a few records
-UPDATE DOCTORS_DEMO SET NAME = 'New Name', PHONE = '555-111-5555' WHERE DOCTOR_ID = 3; -- Update a record
-INSERT INTO DOCTORS_DEMO VALUES -- Insert a few new records
-    (101, 'John Smith', 'john@example.com', '555-222-5555', '123 Fake Street NY NY 10019', 'Cardiology', '2020-01-01'),
-    (102, 'Harry Truman', 'harry@example.com', '555-333-5555', '234 Fake Street NY NY 10019', 'Podiatry', '2023-01-01'),
-    (103, 'Sally Field', 'sally@exmaple.com', '555-444-5555', '345 Fake Street NY NY 10019', 'Surgeon', '2022-01-01');
+DELETE FROM CUSTOMERS WHERE CUSTOMER_ID = 1 OR CUSTOMER_ID = 2; -- Delete a few records
+UPDATE CUSTOMERS SET NAME = 'New Name', PHONE = '555-111-5555' WHERE CUSTOMER_ID = 3; -- Update a record
+INSERT INTO CUSTOMERS VALUES -- Insert a few new records
+    (101, 'John Smith', 'john@example.com', '555-222-5555', '123 Fake Street NY NY 10019', '$5000', '2020-01-01'),
+    (102, 'Harry Truman', 'harry@example.com', '555-333-5555', '234 Fake Street NY NY 10019', '$6000', '2023-01-01'),
+    (103, 'Sally Field', 'sally@exmaple.com', '555-444-5555', '345 Fake Street NY NY 10019', '$99', '2022-01-01');
 
 -- Step 5: Check on data in snowflake table
-SELECT * FROM DOCTORS_DEMO;
+SELECT * FROM CUSTOMERS;
 
 -- Step 6: Check on snowflake sync status
-SELECT * FROM SKYFLOW_PII_STREAM_DOCTORS_DEMO SHOW_CURSOR;
+SELECT * FROM SKYFLOW_PII_STREAM_CUSTOMERS SHOW_CURSOR;
 SELECT STATE, ERROR_MESSAGE, SCHEDULED_TIME
-FROM TABLE(INFORMATION_SCHEMA.TASK_HISTORY(TASK_NAME=>'SKYFLOW_PII_STREAM_DOCTORS_DEMO_TASK'))
+FROM TABLE(INFORMATION_SCHEMA.TASK_HISTORY(TASK_NAME=>'SKYFLOW_PII_STREAM_CUSTOMERS_TASK'))
 ORDER BY SCHEDULED_TIME DESC;
 
 -- Step 7: Check on data in snowflake table, PII has been tokenized automatically
-SELECT * FROM DOCTORS_DEMO;
+SELECT * FROM CUSTOMERS;
